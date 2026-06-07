3242bd9f82bf7be57e7cb315866d2c09e600f7be
Matthewsheldon34 <matthewsheldon34@gmail.com>
2026-06-07 21:33:44 +0300

    Fix deployment config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 import svgr from 'vite-plugin-svgr'
 // https://vite.dev/config/
 export default defineConfig({
+   base: "/matthewsheldon34.github.io/",
   plugins: [
     react(),
     tailwindcss(),
